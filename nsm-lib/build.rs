dd78d2e8feb39549a0031ae434b0c2f49c9e7dcc
Petre Eftime <epetre@amazon.com>
2020-08-21 15:41:19 +0300

    Fixup build
    Move workspace members one level up and move nsm.h file to target
    folder.
    
    Signed-off-by: Petre Eftime <epetre@amazon.com>

--- nsm-lib/build.rs
+++ nsm-lib/build.rs
@@ -5,16 +5,25 @@ extern crate cbindgen;
 
 use cbindgen::Config;
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let crate_env = env::var("CARGO_MANIFEST_DIR").unwrap();
     let crate_path = Path::new(&crate_env);
     let config = Config::from_root_or_default(crate_path);
+    let out_path = output_dir();
     cbindgen::Builder::new()
         .with_crate(crate_path.to_str().unwrap())
         .with_config(config)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("nsm-lib.h");
+        .write_to_file(out_path.join("nsm.h"));
+}
+
+/// Sets target to target/$PROFILE/
+fn output_dir() -> PathBuf {
+    env::var("OUT_DIR")
+        .map(PathBuf::from)
+        .map(|dir| dir.ancestors().nth(3).unwrap().to_owned())
+        .unwrap()
 }
